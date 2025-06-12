4c02786d7a3b51d452e0112c3aa9917c8cc2b6c1
Cykotech <cykotech.development@yahoo.com>
2025-06-12 12:44:04 -0400

    Update Tables model and migrate to DB

--- Backend/RestaurantBackend/Models/Table.cs
+++ Backend/RestaurantBackend/Models/Table.cs
@@ -4,6 +4,11 @@ namespace RestaurantBackend.Models
 	{
 		public int Id { get; set; }
 		public string Status { get; set; } // e.g. Open, Ordered, Closed
-		public List<Order> Orders { get; set; }
+		public string ServerName { get; set; }
+		public int Guests { get; set; }
+		public List<Order> Orders { get; set; } = [];
+		public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+		public List<DateTime> UpdatedAt { get; set; } = [];
+		public DateTime ClosedAt { get; set; }
 	}
 }
