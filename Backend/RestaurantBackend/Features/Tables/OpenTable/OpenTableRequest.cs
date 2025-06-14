cd1d7a6bebefe90fb483630be6ccccac79e2473c
Cykotech <cykotech.development@yahoo.com>
2025-06-14 12:15:07 -0400

    Remove unused directives

--- Backend/RestaurantBackend/Features/Tables/OpenTable/OpenTableRequest.cs
+++ Backend/RestaurantBackend/Features/Tables/OpenTable/OpenTableRequest.cs
@@ -1,5 +1,3 @@
-using MediatR;
-
 namespace RestaurantBackend.Features.Tables.OpenTable
 {
 	public record OpenTableRequest(string ServerName, int Guests);
--- Backend/RestaurantBackend/Features/Tables/OpenTable/OpenTableResponse.cs
+++ Backend/RestaurantBackend/Features/Tables/OpenTable/OpenTableResponse.cs
@@ -1,5 +1,3 @@
-using MediatR;
-
 namespace RestaurantBackend.Features.Tables.OpenTable
 {
 	public record OpenTableResponse(int Id, string ServerName, int Guests, string Status);
