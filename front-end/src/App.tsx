8d44ace9ced6684c5cb39bcc906f0a0b626c394b
Cykotech <cykotech.development@yahoo.com>
2025-06-21 13:06:06 -0400

    Rebase I guess...?

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,24 @@
+import {
+    NavigationMenu,
+    NavigationMenuList,
+    NavigationMenuItem,
+    NavigationMenuLink
+} from "@/components/ui/navigation-menu.tsx";
 import './App.css'
+import {Link} from "react-router";
 
 function App() {
 
-  return (
-    <div>Stuff</div>
-  )
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuLink>
+                        <Link to={"/tables"}>Tables</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>)
 }
 
 export default App
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {BrowserRouter, Route, Routes} from "react-router";
+import TablesPage from "@/pages/TablesPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="tables" element={<TablesPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
