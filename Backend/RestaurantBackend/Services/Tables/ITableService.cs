2a906ed0c8bc19d137e2c55429a28cd154a4227e
Cykotech <cykotech.development@yahoo.com>
2025-11-11 13:41:23 -0500

    Update interface

--- Backend/RestaurantBackend/Services/Tables/ITableService.cs
+++ Backend/RestaurantBackend/Services/Tables/ITableService.cs
@@ -4,9 +4,12 @@ namespace RestaurantBackend.Services.Tables
 {
 	public interface ITableService
 	{
-		Task<List<TableDto>> GetAllTables(bool showClosed);
+		Task<List<TableDto>> GetAllTables(
+			bool showClosed, bool showByServer,
+			int serverId);
+
 		Task<TableDto> GetTableById(int id);
-		Task<TableDto> OpenTable(TableDto table);
+		Task<TableDto> OpenTable(TableDto table, int serverId);
 		Task CloseTable(int id);
 	}
 }
